bfd2d8c746d36bed8a7cccce5534e08553b6ca67
yash vora <officialyashvora1978@gmail.com>
2024-08-12 00:31:09 +0530

    conversion datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,38 @@
+'use strict' // use to state that code is in js newer version
+
+console.log("\nconversion into Number");
+
+let score = "33";
+console.log(typeof score);
+console.log(score);
+console.log(typeof Number(score));
+
+console.log("\nconversion into String");
+
+let score1 = 33;
+console.log(typeof score1);
+console.log(String(score1));
+console.log(typeof String(score1));
+
+console.log("\nconversion into Boolean");
+
+let Score = null;
+console.log(typeof Score);
+console.log(Boolean(Score));
+console.log(typeof Boolean(Score));
+
+console.log("\nconversion into Number for check boolean value");
+
+let isLoggedIn = true
+console.log(typeof isLoggedIn);
+console.log(Number(isLoggedIn));
+console.log(typeof Number(isLoggedIn));
+
+console.log("\nconversion into Boolean for check boolean value");
+
+let isLoggedIn2 = 1
+console.log(typeof isLoggedIn2);
+console.log(Boolean(isLoggedIn2));
+console.log(typeof Boolean(isLoggedIn2));
+
+
